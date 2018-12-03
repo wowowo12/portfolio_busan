5573beed62e99e28bac70287befc647ed41d241c
wowowo12 <finlinfin@gmail.com>
2018-12-03 16:44:04 +0900

    181203

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,21 @@
 $('.movie_bg').tubular({videoId:'SC2zteloma4'});
+
+$(".artist>li").hover(function(){
+    $(this).find(".img").css({"transform":"scale(1.2)","opacity":"0.6"});
+    $(this).find(".art_logo").stop().fadeIn(100);
+},function(){
+    $(this).find(".img").css({"transform":"scale(1)","opacity":"1",});
+    $(this).find(".art_logo").stop().fadeOut(100);
+});
+
+function autoHeight(){
+    $(".hei_wrap").imagesLoaded().done(heiCalc);
+    $(window).resize(heiCalc);
+    function heiCalc(pa, me){
+        $(".hei_wrap").each(function(){
+            $(this).height($(this).find(".hei_elem").height());
+        });
+       
+    }
+}
+autoHeight();
