80e58f2e2658074565a83913477b22d50cc05996
wowowo12 <finlinfin@gmail.com>
2019-02-01 17:37:57 +0900

    190201

--- public/js/main.js
+++ public/js/main.js
@@ -28,3 +28,45 @@ $("#time_table .btn").click(function(){
     $(".time_lt").css({"display":"block"});
 });
 }
+
+$(window).scroll(function(){
+    $(".samrock").addClass("samrock_ani");
+});
+var scTop = $(window).scrollTop();
+console.log(scTop);
+var gap = [];
+var now = 0;
+$(window).resize(function(){
+    $(".page").each(function(i){
+        gap[i] = $(this).offset().top;
+    });
+}).trigger("resize");
+$(window).on("mousewheel DOMMouseScroll", wheelFn);
+function wheelFn(e){
+    var dir = e.originalEvent.wheelDelta;
+    scTop= $(window).scrollTop();
+
+    for(var i = 0; i <gap.length; i++){
+        if(scTop <= gap[i]){
+            now= i;
+            break;
+        }
+    }
+    if(dir>0){
+        if(now>0)now--;
+    } else{
+        if (now< gap.length - 1) now++;
+     }
+     if(now=4){
+         $(".news_img1").addClass("news_img1_ani");  
+         $(".news_img2").addClass("news_img2_ani");       
+     }
+   
+     if (scTop + 300 > gap[0]) {
+        $(".ab_ban_txt>hr:nth-of-type(1)").addClass("samrock_hr_ani");
+        $(".ab_ban_txt>hr:nth-of-type(2)").addClass("samrock_hr2_ani");
+        $(".ab_ban_txt>h2:nth-of-type(1)").addClass("samrock_txt_ani");
+        $(".ab_ban_txt>h2:nth-of-type(2)").addClass("samrock_txt_ani");
+        $(".ab_ban_txt>h2:nth-of-type(3)").addClass("samrock_txt_ani");
+    }
+}
